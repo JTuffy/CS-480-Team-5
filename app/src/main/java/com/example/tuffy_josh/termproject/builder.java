f6a23945a0c9a829d531fee607df9222f2bb2e55
tomoppenheim <oppenhe_thom@bentley.edu>
2017-04-21 13:25:19 -0400

    Updated builder activity
    added options menu, added text to speach, removed two buttons

--- app/src/main/java/com/example/tuffy_josh/termproject/builder.java
+++ app/src/main/java/com/example/tuffy_josh/termproject/builder.java
@@ -1,8 +1,11 @@
 package com.example.tuffy_josh.termproject;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import  android.widget.ArrayAdapter;
 import  android.widget.ListView;
@@ -10,6 +13,8 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
+
 import  android.widget.RadioGroup;
 import  android.widget.RadioButton;
 import  android.widget.RadioGroup.OnCheckedChangeListener;
@@ -20,12 +25,21 @@ public class builder extends AppCompatActivity {
     ArrayAdapter<String> arrayAdapter;
     private EditText dataEntry;
     String[] items = {};
+    private TextToSpeech speaker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_builder);
 
+        speaker = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    speaker.setLanguage(Locale.US);
+                }
+            }
+        });
 
         dataEntry = (EditText)findViewById(R.id.editText);
         ListView listview = (ListView)findViewById(R.id.list);
@@ -40,21 +54,62 @@ public class builder extends AppCompatActivity {
         });
     }
 
-    public void enterClicked(View v){
-        String newEntry = dataEntry.getText().toString();
-        arrayList.add(newEntry);
-        arrayAdapter.notifyDataSetChanged();
-        dataEntry.setText("");
+    public void speak(String output){
+        speaker.speak(output, TextToSpeech.QUEUE_FLUSH, null, "id 0");
+
     }
 
-    public void deleteClicked(View v){
-        String deleteEntry = dataEntry.getText().toString();
-        arrayList.remove(deleteEntry);
-        arrayAdapter.notifyDataSetChanged();
-        dataEntry.setText("");
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        menu.add(1, 1, 1, "Enter");
+        menu.add(1,2,2,"Delete");
+        menu.add(1,3,3,"Clear");
+
+        return true;
     }
 
     public void goClicked(View v){
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case 1:
+                if (arrayList.size() < 6) {
+
+                    String newEntry = dataEntry.getText().toString();
+                    newEntry = newEntry.toUpperCase();
+                    arrayList.add(newEntry);
+                    arrayAdapter.notifyDataSetChanged();
+                    dataEntry.setText("");
+                    String addSpeak = newEntry + " was added";
+                    speak(addSpeak);
+                }
+            return true;
+
+            case 2:
+                String deleteEntry = dataEntry.getText().toString();
+                deleteEntry = deleteEntry.toUpperCase();
+                arrayList.remove(deleteEntry);
+                arrayAdapter.notifyDataSetChanged();
+                dataEntry.setText("");
+                String deleteSpeak = deleteEntry + " was deleted";
+                speak(deleteSpeak);
+            return true;
+
+            case 3:
+                arrayList.clear();
+                arrayAdapter.notifyDataSetChanged();
+                String clearSpeak = "Schedule cleared";
+                speak(clearSpeak);
+
+            default: super.onOptionsItemSelected(item);
+        }
+        return false;
+    }
+
 }
